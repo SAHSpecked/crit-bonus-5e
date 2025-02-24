f9741c03c233ea2daa9470ca507e982ee0effcf2
SAHSpecked <SAHSpecked@gmail.com>
2025-02-23 20:12:08 -0500

    Custom messages

--- scripts/module.js
+++ scripts/module.js
@@ -29,6 +29,36 @@ Hooks.on("init", () => {
       console.log("Crit Bonus Value: " + value);
     },
   });
+
+  game.settings.register(MODULE_ID, "naturalTwentyMessage", {
+    name: "Natural Twenty Message",
+    hint: "The message, preceding the Critical Total, on a natural twenty.",
+    scope: "world", // "world" = sync to db, "client" = local storage
+    config: true,
+    type: String,
+    default: "Natural twenty!",
+    onChange: (value) => {
+      // value is the new value of the setting
+      const vt = value.trim();
+      game.settings.set(MODULE_ID, "naturalTwentyMessage", vt);
+      console.log("Natural Twenty Message: " + vt);
+    },
+  });
+
+  game.settings.register(MODULE_ID, "naturalOneMessage", {
+    name: "Natural One Message",
+    hint: "The message on a natural one.",
+    scope: "world", // "world" = sync to db, "client" = local storage
+    config: true,
+    type: String,
+    default: "Natural one. Oof...",
+    onChange: (value) => {
+      // value is the new value of the setting
+      const vt = value.trim();
+      game.settings.set(MODULE_ID, "naturalOneMessage", vt);
+      console.log("Natural One Message: " + vt);
+    },
+  });
 });
 
 async function checkRollForNaturals(roll) {
@@ -47,11 +77,13 @@ async function checkRollForNaturals(roll) {
       if (rollValue == 20) {
         const rollModified = roll._total + game.settings.get(MODULE_ID, "critBonusValue");
         await ChatMessage.create({
-          content: `Natural twenty! Critical Total: <b>${rollModified}</b>`,
+          speaker: { alias: "Critical Confirmation" },
+          content: `${game.settings.get(MODULE_ID, "naturalTwentyMessage")}<br><br>Critical Total: <b>${rollModified}</b>`,
         });
       } else if (rollValue == 1) {
         await ChatMessage.create({
-          content: `Natural one. Oof...`,
+          speaker: { alias: "Critical Confirmation" },
+          content: `${game.settings.get(MODULE_ID, "naturalOneMessage")}`,
         });
       }
     }
@@ -59,7 +91,7 @@ async function checkRollForNaturals(roll) {
 }
 
 async function checkRollForNaturalsV2(rolls) {
-  if (game.system.version >= "4.1.0" && game.system.version < "5.0.0") {
+  if (game.system.version >= "4.1.0") {
     if (game.settings.get(MODULE_ID, "enableCritBonus")) {
       // Get active die number
       let rollValue = null;
@@ -74,11 +106,13 @@ async function checkRollForNaturalsV2(rolls) {
       if (rollValue == 20) {
         const rollModified = rolls[0].total + game.settings.get(MODULE_ID, "critBonusValue");
         await ChatMessage.create({
-          content: `Natural twenty! Critical Total: <b>${rollModified}</b>`,
+          speaker: { alias: "Critical Confirmation" },
+          content: `${game.settings.get(MODULE_ID, "naturalTwentyMessage")}<br><br>Critical Total: <b>${rollModified}</b>`,
         });
       } else if (rollValue == 1) {
         await ChatMessage.create({
-          content: `Natural one. Oof...`,
+          speaker: { alias: "Critical Confirmation" },
+          content: `${game.settings.get(MODULE_ID, "naturalOneMessage")}`,
         });
       }
     }
